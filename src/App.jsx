5fa5e19cc5f7c3ac8186f79a38a337132a216c6a
Dhruvi Patel <dp7070dp@gmail.com>
2024-08-08 10:42:48 -0400

    intital project and UI setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+      style={{backgroundImage:`url(https://images.pexels.com/photos/4495803/pexels-photo-4495803.jpeg)` }}
+    >
+     </div>
+    </>
+  )
+}
+
+export default App
